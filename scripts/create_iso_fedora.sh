c5d96e314f1ce8e65b2d12777b22e5cecaac631f
David E <davide135@gmail.com>
2018-04-18 20:47:21 +0200

    add script for fedora

---
+++ scripts/create_iso_fedora.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+build_dir="/home/david/Playground/fedora_isos/build"
+MIRROR_URL="https://ftp.halifax.rwth-aachen.de/fedora/linux/releases/27/Server/x86_64/os/"
+#if [ ! -d "${build_dir}/isolinux" ]; then
+	echo "##### Start Downloading #####"
+  cd /tmp
+	wget -nv -r --accept-regex=$MIRROR_URL/isolinux/* $MIRROR_URL/isolinux/
+	wget -nv -r --accept-regex=$MIRROR_URL/images/* $MIRROR_URL/images/
+	#wget -nv -r --accept-regex=$MIRROR_URL/LiveOS/* $MIRROR_URL/LiveOS/
+
+	# remove index files
+	find . -type f -name index.html\* -exec rm {} \;
+
+	mirror_path="/tmp/`echo $MIRROR_URL | sed 's/https\?:\/\///'`"
+
+	mv ${mirror_path}/isolinux ${build_dir}/
+	mv ${mirror_path}/images ${build_dir}/isolinux/
+	#mv ${mirror_path}/LiveOS ${build_dir}/isolinux/
+	rm -r --interactive=never ${mirror_path}
+  cd ${build_dir}
+#fi
+
+
+if [ $HOST ]
+then
+	host=$HOST
+else
+	host="anaconda-ks"
+fi
+
+# include kickstart config
+mkdir -p ${build_dir}/isolinux/config
+cp ${host}.cfg ${build_dir}/isolinux/config/ks.cfg
+
+cp /scripts/isolinux.cfg ${build_dir}/isolinux/isolinux.cfg
+
+chmod 664 ${build_dir}/isolinux/isolinux.bin
+
+mkisofs -o ${host}.iso -b isolinux.bin -c boot.cat -no-emul-boot \
+  -V 'CentOS 7 x86_64' \
+  -boot-load-size 4 -boot-info-table -R -J -v -T isolinux/
